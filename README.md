ac34797251df34949e035045318e673b1a39eac3
Aja Apps <aja@server.fake>
2013-06-23 17:31:55 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+Widget to toggle flight mode.
+
+This simple widget toggles flight mode of your device.  Just put the widget on
+your home screen and touch it to turn on (or turn off) flight mode.
+
+![Example screen](img/screencap_off_small.png) 
+![Example screen](img/screencap_on_small.png)
+
+Note: this widget will not work on Android 4.2 and above. 
+
+Copyright © 2013 Aja Apps.  This work is free. You can redistribute it and/or
+modify it under the terms of the Do What The Fuck You Want To Public License,
+Version 2, as published by Sam Hocevar. See http://www.wtfpl.net/ for more
+details.
+
+Source code and issue tracker reside at https://github.com/ajaapps/flightmode.
+
+
---
+++ src/org/aja/flightmode/FlightmodeAppWidgetProvider.java
@@ -0,0 +1,99 @@
+package org.aja.flightmode;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.RemoteViews;
+
+public class FlightmodeAppWidgetProvider extends AppWidgetProvider {
+
+  static final String        LOGTAG = "org.aja.flightmode";
+  static final String        TOGGLE_REQUEST = "org.aja.flightmode.ACTION_TOGGLE_REQUEST";
+  static final ComponentName NAME_OF_THIS_COMPONENT = new ComponentName( 
+      "org.aja.flightmode",
+      "org.aja.flightmode.FlightmodeAppWidgetProvider");
+
+  /**
+   * Receives and processes a button pressed intent or state change.
+   */
+  @Override
+    public void onReceive(Context cntxt, Intent ntnt) {
+      PLog.d(LOGTAG, "onReceive()  (ntnt action is: " + ntnt.getAction() + ")");
+      super.onReceive(cntxt, ntnt);
+      if (ntnt.getAction().equals(TOGGLE_REQUEST)) {
+        toggleFlightmode(cntxt);
+      } 
+      else if (ntnt.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+        updateWidgets(cntxt); 
+      }
+    }
+
+  /**
+   * Toggles flighmode by changing the corresponding system setting.
+   * (No update of the widgets done here: we wait for te broadcast intent
+   *  to trigger our onReceive().)
+   */
+  private void toggleFlightmode(Context cntxt) {
+    PLog.d(LOGTAG, "toggleFlightmode()");
+
+    // toggle the system setting
+    int oldvalue = Settings.System.getInt( cntxt.getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON, 0);
+    int newvalue = oldvalue == 1 ? 0 : 1 ;
+    Settings.System.putInt( cntxt.getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON, 
+        newvalue );
+
+    // broadcast the fact
+    Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+    intent.putExtra("state", newvalue);
+    cntxt.sendBroadcast(intent);
+
+  }
+
+  /**
+   * Initialize the widgets. 
+   * Called only when instantiating a new widget, as updateMillis="0".
+   */
+  @Override
+    public void onUpdate(Context cntxt, AppWidgetManager a_mgr, int[] ids) {
+      PLog.d(LOGTAG,"onUpdate()");
+      super.onUpdate(cntxt, a_mgr, ids);
+      updateWidgets(cntxt);
+    }
+
+  /**
+   * Update all widgets belonging to this provider.
+   */
+  public static void updateWidgets(Context cntxt) {
+    PLog.d(LOGTAG, "updateWidgets()");
+
+    // get the RemoteViews object containing the view objects in the widget layout
+    RemoteViews rviews = new RemoteViews(cntxt.getPackageName(), R.layout.main);
+
+    // link a pending intent to the button view
+    Intent ntnt = new Intent(TOGGLE_REQUEST);
+    PendingIntent pi = PendingIntent.getBroadcast(cntxt,0,ntnt,0);
+    rviews.setOnClickPendingIntent(R.id.btn_flightmode, pi);
+
+    // set the right looks for the button
+    int status = Settings.System.getInt( cntxt.getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON, 0); 
+    int drwbl = R.drawable.ic_flightmode_off;
+    if ( status == 1 ) {
+      drwbl = R.drawable.ic_flightmode_on;
+    };
+    rviews.setImageViewResource(R.id.img_flightmode, drwbl);
+
+    // Use rviews to be the RemoteViews object in all AppWidget instances for this provider
+    final AppWidgetManager a_mgr = AppWidgetManager.getInstance(cntxt);
+    a_mgr.updateAppWidget(NAME_OF_THIS_COMPONENT, rviews);
+
+  }
+
+}
